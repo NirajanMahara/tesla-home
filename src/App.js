146945269b9db29872c1986755a2c35fe4e23c9e
Nirajan Mahara <np03a190125@heraldcollege.edu.np>
2021-05-18 13:49:13 +0545

    Header + assests ✨💼

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tesla Home</h1>
+      <Header />
+      {/* item => snapping */}
+      {/* item => snapping */}
+      {/* item => snapping */}
+      {/* item => snapping */}
     </div>
   );
 }
